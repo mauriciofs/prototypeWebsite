be61e6788f46556804fca8ea739c785266c3443c
mauriciofs <mauriciofs6@gmail.com>
2016-05-12 23:08:47 +0100

    Adding DI examples

--- src/public/index.php
+++ src/public/index.php
@@ -11,8 +11,23 @@ $app = new \Slim\App(["settings" => $config]);
 
 // Define app routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
+    $this->logger->addInfo("Something interesting happened");
     return $response->write("Hello " . $args['name']);
 });
+
+//DI
+$container = $app->getContainer();
+$container['logger'] = function($c) {
+    //Check if directory exists first
+    if (!is_dir(dirname(__FILE__) . '/../logs')){
+        mkdir(dirname(__FILE__) . '/../logs');
+    }
+    
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
 
 // Run app
 $app->run();
