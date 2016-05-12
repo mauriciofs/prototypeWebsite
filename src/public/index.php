0238aa17498dc85d23280547635e809837db31c6
mauriciofs <mauriciofs6@gmail.com>
2016-05-12 22:56:55 +0100

    Updating autoload file

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,13 @@
 <?php
+
+require dirname(__DIR__) . "/../vendor/autoload.php";
+
+$config = array(
+    'display_errors' => true
+);
+
 // Create and configure Slim app
-$app = new \Slim\App;
+$app = new \Slim\App(["settings" => $config]);
 
 // Define app routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
